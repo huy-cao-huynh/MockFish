083d7f916a2dd0577293aa7adb00d2c99bc5c78f
Huy Huynh <huycaohuynh@gmail.com>
2024-09-06 14:35:28 -0700

    Updates: - Added the AI to the GUI - Still need to debug some errors in play - Need to edit the AI to remove hardcoding - Need to write testcases for all of my code

--- JChess/src/com/chess/JChess.java
+++ JChess/src/com/chess/JChess.java
@@ -11,6 +11,8 @@ public class JChess {
     public static void main(String[] args) {
         Board board = Board.createStandardBoard();
 
-        Table table = new Table();
+        System.out.println(board);
+
+        Table.get().show();
     }
 }
--- JChess/src/com/chess/engine/player/ai/StandardBoardEvaluator.java
+++ JChess/src/com/chess/engine/player/ai/StandardBoardEvaluator.java
@@ -1,30 +1,51 @@
 package com.chess.engine.player.ai;
 
 import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
 import com.chess.engine.pieces.Piece;
 import com.chess.engine.player.Player;
 
 public final class StandardBoardEvaluator implements BoardEvaluator {
 
-    private static final int CHECK_BONUS = 50;
+    private static final int CHECK_BONUS = 45;
     private static final int CHECK_MATE_BONUS = 10000;
     private static final int DEPTH_BONUS = 100;
-    private static final int CASTLE_BONUS = 60;
+    private static final int CASTLE_BONUS = 25;
+    private static final int MOBILITY_MULTIPLIER = 5;
+    private static final int ATTACK_MULTIPLIER = 1;
 
     @Override
     public int evaluate(Board board,
                         int depth) {
-        return scorePlayer(board, board.whitePlayer(), depth) - scorePlayer(board, board.blackPlayer(), depth);
+        return scorePlayer(board.whitePlayer(), depth) - scorePlayer(board.blackPlayer(), depth);
     }
 
-    private int scorePlayer(final Board board,
-                            final Player player,
+    private int scorePlayer(final Player player,
                             final int depth) {
         return pieceValue(player) +
                 mobility(player) +
-                check(player) +
-                checkmate(player, depth) +
+                kingThreats(player, depth) +
+                attacks(player) +
                 castled(player);
+    }
+
+    private static int kingThreats(final Player player,
+                                   final int depth) {
+        return player.getOpponent().isInCheckMate() ? checkmate(player, depth) : check(player);
+    }
+
+    private static int attacks(final Player player) {
+        int attackScore = 0;
+        for(final Move move : player.getLegalMoves()) {
+            if(move.isAttack()) {
+                final Piece movedPiece = move.getMovedPiece();
+                final Piece attackedPiece = move.getAttackedPiece();
+                if(movedPiece.getPieceValue() <= attackedPiece.getPieceValue()) {
+                    attackScore++;
+                }
+            }
+        }
+        return attackScore * ATTACK_MULTIPLIER;
     }
 
     private static int castled(final Player player) {
@@ -44,7 +65,11 @@ public final class StandardBoardEvaluator implements BoardEvaluator {
     }
 
     private static int mobility(final Player player) {
-        return player.getLegalMoves().size();
+        return MOBILITY_MULTIPLIER * mobilityRatio(player);
+    }
+
+    private static int mobilityRatio(final Player player) {
+        return (int)((player.getLegalMoves().size() * 10.0f) / player.getOpponent().getLegalMoves().size());
     }
 
     private static int pieceValue(final Player player) {
